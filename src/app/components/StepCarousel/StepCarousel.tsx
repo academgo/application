b640027466d06a81d283200057b0e4d44b5c953c
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-07-28 16:06:10 +0200

    add marquee steps

--- src/app/components/StepCarousel/StepCarousel.tsx
+++ src/app/components/StepCarousel/StepCarousel.tsx
@@ -1,34 +1,20 @@
 "use client";
 import React, { FC, useEffect, useRef, ReactNode } from "react";
 import styles from "./StepCarousel.module.scss";
+import Marquee from "react-fast-marquee";
 
 type Props = {
   number: string;
   title: string;
   children: ReactNode;
-  style?: any;
 };
 
-const StepCarousel: FC<Props> = ({ children, style }) => {
-  const carouselRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const carousel = carouselRef.current;
-    if (carousel) {
-      const items = Array.from(carousel.children);
-      items.forEach(item => {
-        const clone = item.cloneNode(true);
-        carousel.appendChild(clone);
-      });
-    }
-  }, []);
-
+const StepCarousel: FC<Props> = ({ children }) => {
   return (
-    <div style={style} className={styles.wrapper}>
-      <div className={styles.stepCarousel} ref={carouselRef}>
+    <div className={styles.stepCarousel}>
+      <Marquee loop={0} autoFill={true}>
         {children}
-        {children}
-      </div>
+      </Marquee>
     </div>
   );
 };
