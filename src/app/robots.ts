a8e59b871b715850fffa10c8bd8eab87ef340600
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-07-29 15:37:54 +0200

    open index

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      // allow: "/",
-      disallow: "/"
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/ru/success", "/en/success"]
+      }
+    ],
     sitemap: "https://academgo.com/sitemap.xml"
   };
 }
