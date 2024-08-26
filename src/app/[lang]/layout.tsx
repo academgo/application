8b4be20d2158c3fbef2db593a2dcbc8e486d571e
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-08-26 19:37:56 +0200

    add new search console

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import { ModalProvider } from "../context/ModalContext";
 import { Suspense } from "react";
 import { FacebookPixelEvents } from "../components/pixel-events";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,6 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="y26kx-fqwQmu8vSsuIo8zW09MIp0pnOQNHnGFNggnmQ"
+        />
+      </Head>
       <body className={inter.className}>
         <ModalProvider>{children}</ModalProvider>
         <GoogleAnalytics gaId="G-XTMLVRC9RR" />
