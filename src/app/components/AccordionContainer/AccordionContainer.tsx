f35bd7ff43e7776b7d5429e075236236ee1daa62
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2024-11-10 23:31:28 +0100

    hide schema faq

--- src/app/components/AccordionContainer/AccordionContainer.tsx
+++ src/app/components/AccordionContainer/AccordionContainer.tsx
@@ -10,39 +10,12 @@ type AccordionContainerProps = {
 const AccordionContainer: React.FC<AccordionContainerProps> = ({ block }) => {
   const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
 
-  // Создаем JSON-LD разметку для FAQ на основе данных блока
-  const faqSchema = {
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    mainEntity: block.items.map((item: any) => ({
-      "@type": "Question",
-      name: item.title,
-      acceptedAnswer: {
-        "@type": "Answer",
-        text: item.content
-          .map((block: any) =>
-            block.children
-              ? block.children.map((child: any) => child.text || "").join("")
-              : ""
-          )
-          .join(" ")
-      }
-    }))
-  };
-
   return (
-    <div>
-      {/* Вставляем JSON-LD разметку для FAQ */}
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
-      />
-      <AccordionBlockComponent
-        block={block}
-        expandedIndex={expandedIndex}
-        setExpandedIndex={setExpandedIndex}
-      />
-    </div>
+    <AccordionBlockComponent
+      block={block}
+      expandedIndex={expandedIndex}
+      setExpandedIndex={setExpandedIndex}
+    />
   );
 };
 
