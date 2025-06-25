714084b235f000d143384fdda79e0d5f5ddfb4c6
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-06-25 10:37:21 +0200

    edit redirects

--- next.config.mjs
+++ next.config.mjs
@@ -33,108 +33,23 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/about',
-        destination: '/en',
+        source: '/blog/cost-of-studying-in-poland',
+        destination: '/cost-of-studying-in-poland',
         permanent: true,
       },
       {
-        source: '/why-poland',
-        destination: '/en',
+        source: '/blog/how-to-apply-to-university-in-poland',
+        destination: '/study-in-poland',
         permanent: true,
       },
       {
-        source: '/universities',
-        destination: '/en',
+        source: '/blog/studying-in-poland',
+        destination: '/study-in-poland',
         permanent: true,
       },
       {
-        source: '/contact',
-        destination: '/en',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/universitety-v-polshe-dla-inostrancev',
-        destination: '/ru/vuzy-polshi',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/stoimost-obucheniya-v-polshe',
-        destination: '/ru/stoimost-obucheniya-v-polshe ',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-dlya-belorusov',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-dlya-belorusov-v-polshe ',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-dlya-kazakhstancev',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-v-polshe-dlya-kazakhstancev ',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-dlya-ukraincev',
-        destination: '/ru/obuchenie-v-polshe/ucheba-v-polshe-dlya-ukraincev',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-s-kartoi-polyaka',
-        destination: '/ru/obuchenie-v-polshe/postuplenie-v-polshu-po-karte-polyaka',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/studencheskaya-viza-v-polshu-dlya-belorusov',
-        destination: '/ru/studencheskaya-viza/studencheskaya-viza-v-polshu-dlya-belorusov',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/yuridicheskoe-obrazovanie-v-polshe',
-        destination: '/ru/vuzy-polshi/yuridicheskoe-obrazovanie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/studencheskaya-viza-v-polshu-dlya-ukraincev',
-        destination: '/ru/studencheskaya-viza/studencheskaya-viza-v-polshu-dlya-ukraincev',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/polushenie-studencheskoi-vizy',
-        destination: '/ru/studencheskaya-viza',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/kak-postupit-v-polshu ',
-        destination: '/ru/obuchenie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/obuchenie-v-polshe',
-        destination: '/ru/obuchenie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-v-2024-godu',
-        destination: '/ru/obuchenie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/cost-of-studying-in-poland-for-international-students',
-        destination: '/blog/cost-of-studying-in-poland',
-        permanent: true,
-      },
-      {
-        source: '/how-to-apply-to-university-in-poland',
-        destination: '/blog/how-to-apply-to-university-in-poland',
-        permanent: true,
-      },
-      {
-        source: '/studying-in-poland',
-        destination: '/blog/studying-in-poland',
-        permanent: true,
-      },
-      {
-        source: '/student-visa-in-poland-how-to-get-a-student-visa-in-poland',
-        destination: '/blog/universities-in-poland-for-international-students',
+        source: '/blog/universities-in-poland-for-international-students',
+        destination: '/universities-in-poland',
         permanent: true,
       },
       {
@@ -143,18 +58,18 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/specialties-in-polish-universities',
-        destination: '/blog/specialties-in-polish-universitiesv',
+        source: '/blog/specialties-in-polish-universities',
+        destination: 'study-in-poland/degree-programs-in-polish-universities',
         permanent: true,
       },
       {
-        source: '/scholarships-in-poland-how-to-get-a-scholarship-in-poland',
-        destination: '/blog/how-to-get-a-scholarship-in-poland',
+        source: '/blog/how-to-get-a-scholarship-in-poland',
+        destination: '/study-in-poland/scholarships-for-studying-in-poland',
         permanent: true,
       },
       {
-        source: '/study-in-poland-2024-admission-in-eu-2024',
-        destination: '/blog/study-in-poland-2024-or-admission-in-eu-2024',
+        source: '/blog/study-in-poland-2024-or-admission-in-eu-2024',
+        destination: '/study-in-poland',
         permanent: true,
       },
       {
@@ -183,8 +98,8 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/study-in-poland-from-vietnam',
-        destination: '/blog/study-in-poland-from-vietnam',
+        source: '/blog/study-in-poland-from-vietnam',
+        destination: '/study-in-poland/study-in-poland-from-vietnam',
         permanent: true,
       },
       {
@@ -193,113 +108,13 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/ru/blog/besplatnoe-obrazovanie-v-polshe-dlya-kazakhstancev',
-        destination: '/ru/obuchenie-v-polshe/besplatnoe-obrazovanie-v-polshe-dlya-kazakhstancev',
+        source: '/blog/cost-of-studying-in-poland',
+        destination: '/cost-of-studying-in-poland',
         permanent: true,
       },
       {
-        source: '/ru/blog/stoimost-obucheniya-v-polshe',
-        destination: '/ru/stoimost-obucheniya-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-dlya-belorusov',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-dlya-belorusov-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/postuplenie-v-polshu-dlya-kazakhstancev',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-v-polshe-dlya-kazakhstancev',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/studencheskaya-viza-v-polshu-dlya-ukraincev',
-        destination: '/studencheskaya-viza/studencheskaya-viza-v-polshu-dlya-ukraincev',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/medicinskoe-obrazovanie-v-polshe',
-        destination: '/ru/obuchenie-v-polshe/medicinskoe-obrazovanie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/it-obrazovanie-v-polshe',
-        destination: '/ru/obuchenie-v-polshe/it-obrazovanie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/obuchenie-v-polshe-dlya-kazakhstancev',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-v-polshe-dlya-kazakhstancev',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe',
-        destination: '/ru/obuchenie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/obuchenie-dlya-belorusov-v-polshe',
-        destination: '/ru/obuchenie-v-polshe/obuchenie-dlya-belorusov-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/postuplenie-v-polshu-po-karte-polyaka',
-        destination: '/ru/obuchenie-v-polshe/postuplenie-v-polshu-po-karte-polyaka',
-        permanent: true,
-      },
-      {
-        source: '/studencheskaya-viza/studencheskaya-viza-v-polshu-dlya-kazakhstancev',
-        destination: '/ru/studencheskaya-viza/studencheskaya-viza-v-polshu-dlya-kazakhstancev',
-        permanent: true,
-      },
-      {
-        source: '/vuzy-polshi',
-        destination: '/ru/vuzy-polshi',
-        permanent: true,
-      },
-      {
-        source: '/obuchenie-v-polshe/ucheba-v-polshe-dlya-ukraincev',
-        destination: '/ru/obuchenie-v-polshe/ucheba-v-polshe-dlya-ukraincev',
-        permanent: true,
-      },
-      {
-        source: '/vuzy-polshi/yuridicheskoe-obrazovanie-v-polshe',
-        destination: '/ru/vuzy-polshi/yuridicheskoe-obrazovanie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/10-prichin-uchitsya-v-polshe',
-        destination: '/ru/obuchenie-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/specialnosti-v-universitetakh-polshi',
-        destination: '/ru/obuchenie-v-polshe/specialnosti-v-universitetakh-polshi',
-        permanent: true,
-      },
-      {
-        source: '/ru/blog/stoimost-obucheniya-v-polshe-dlya-inostrancev',
-        destination: '/ru/stoimost-obucheniya-v-polshe',
-        permanent: true,
-      },
-      {
-        source: '/stoimost-uslug',
-        destination: '/ru/stoimost-uslug',
-        permanent: true,
-      },
-      {
-        source: '/index.php',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/index.htm',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/index.html',
-        destination: '/',
+        source: '/blog/student-visa-in-poland-how-to-get-a-student-visa-in-poland',
+        destination: '/student-visa-to-poland',
         permanent: true,
       },
       // Удаление завершающего слеша, исключая корень сайта
