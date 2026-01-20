299c1efcacf184a74115fa8035074b643742cfeb
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2026-01-20 11:21:18 +0100

    latest npm

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -1,10 +1,9 @@
-"use client";
+import dynamicImport from "next/dynamic";
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../sanity.config";
+export const dynamic = "force-static";
 
-// export const dynamic = 'force-static';
+const Studio = dynamicImport(() => import("../Studio"), { ssr: false });
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
+export default function StudioCatchAllPage() {
+  return <Studio />;
 }
